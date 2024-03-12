1575bb146fb66982f0de85357c75774d733e1e24
Nikol Dimitrova <nikoldimitrova@bulb.co.uk>
2024-03-12 19:21:01 +0000

    adding the pagination stuff

--- django_graphql_sports/app/helpers.py
+++ django_graphql_sports/app/helpers.py
@@ -1,43 +1,24 @@
 import requests
 
-def get_football_games(competition_id, page=1, page_size=40):
-    url = f"https://api.football-data.org/v4/competitions/{competition_id}/matches"
+def get_football_games(competition_id):
+    url = f"https://api.football-data.org/v4/competitions/{competition_id}/matches?"
     headers = {"X-Auth-Token": "ea32770561614c50b01e46b75c957eee"}
-    params = {"page": page, "page_size": page_size}
 
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         return response.json()['matches']
+    
     else:
         return []
     
-# def get_nba_games():
-#     url = "http://api.balldontlie.io/v1/games"
-#     headers = {"Authorization": "b2a89977-91d6-4f8b-9075-583f7fe20edc"}  #the api key shouldn't be here 
-#     params={"per_page": 40}
-
-#     response = requests.get(url, headers=headers, params=params)
-#     if response.status_code == 200:
-#         data = response.json()
-#         return data
-#     else:
-#         return None, None
-
-def get_paginated_players(page=1, per_page=25):
-    all_players = []
+def get_nba_games():
     url = "http://api.balldontlie.io/v1/games"
     headers = {"Authorization": "b2a89977-91d6-4f8b-9075-583f7fe20edc"}  #the api key shouldn't be here 
-    params = {"page": page, "per_page": per_page}
-    
-    while True:
-        response = requests.get(url, headers=headers, params=params)
+    params={"per_page": 40}
+
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
         data = response.json()
-        if not data["data"]:
-            break
-        all_players.extend(data["data"])
-        
-        # Update pagination params for the next page
-        next_cursor = data["meta"]["next_cursor"]
-        params["page"] = next_cursor
-    
-    return all_players
+        return data
+    else:
+        return None, None
